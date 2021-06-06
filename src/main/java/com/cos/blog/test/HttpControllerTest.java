2b7511838abf1ed1d0d0bb4c607e63a557fa8c63
tjdans345 <tjdans34568@gmail.com>
2021-06-06 17:30:25 +0900

    http 요청 Test2

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -1,5 +1,47 @@
 package com.cos.blog.test;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// 사용자가 요청 -> 응답 (HTML 파일)
+// @Controller
+
+// 사용자가 요청 -> 응답(Data를 응답 해줌) RestController이다
+
+@RestController
 public class HttpControllerTest {
+	
+	// 인터넷 브라우저 요청은 get요청밖에 할 수 없다.
+	// http://localhost:8090/http/get (select)
+	@GetMapping("/http/get")
+	public String getTest(Member m) {
+		
+		return "get 요청"+m.getId() +", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
+	}
+	
+	// http://localhost:8090/http/post (insert)
+	@PostMapping("/http/post") // text/plain(raw Data) , application/json(raw Data)
+	public String postTest(@RequestBody Member m) { //MessageConverter가 알아서 매핑을 해준다 (스프링부트)
+		return "post 요청:" + m.getId() +", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
+	}
+	
+	// http://localhost:8090/http/put (update)
+	@PutMapping("/http/put")
+	public String putTset(@RequestBody Member m) {
+		return "put 요청" + m.getId() +", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
+	}
+	
+	// http://localhost:8090/http/delete (delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
 
 }
