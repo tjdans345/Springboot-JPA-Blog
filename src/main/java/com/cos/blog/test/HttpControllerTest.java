c49ede2956445912c609c62568fe7caed0afee5d
tjdans345 <tjdans34568@gmail.com>
2021-06-06 19:31:22 +0900

    lombok 세팅

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -18,11 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest :";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter"+m.getUsername());
+		m.setUsername("홍길동");
+		System.out.println(TAG+"getter"+m.getUsername());
+		return "lombok test 완료";
+	}
+	
+	
 	// 인터넷 브라우저 요청은 get요청밖에 할 수 없다.
 	// http://localhost:8090/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
 		
+		System.out.println(TAG + "getter: " + m.getId());
+		m.setId(5000);
+		System.out.println(TAG + "setter:" + m.getId());
 		return "get 요청"+m.getId() +", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
@@ -31,7 +46,7 @@ public class HttpControllerTest {
 	public String postTest(@RequestBody Member m) { //MessageConverter가 알아서 매핑을 해준다 (스프링부트)
 		return "post 요청:" + m.getId() +", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
-	
+
 	// http://localhost:8090/http/put (update)
 	@PutMapping("/http/put")
 	public String putTset(@RequestBody Member m) {
