515e6fff0f0467a33e2225bde677c2fb851b4a08
tjdans345 <tjdans34568@gmail.com>
2021-07-31 17:00:10 +0900

    Exception 처리 및 Json 파싱

---
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice // Exception이 발생하면 이 클래스로 오게 만드는 어노테이션
+@RestController
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value=IllegalArgumentException.class)
+	public String handleArgumentException(IllegalArgumentException e) {
+		return "<h1>"+e.getMessage()+"<h1>";
+	}
+}
