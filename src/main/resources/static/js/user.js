63cd4f1e9ce41a5d216df42cef50cc4ccdbe7920
tjdans345 <tjdans34568@gmail.com>
2021-07-31 21:56:03 +0900

    js파일 구현중

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,7 +1,7 @@
 let index = {	
 	
 	init: function() {
-		$("#btn-save").on("click", ()=> {
+		$("#btn-save").on("click", ()=> { // function(){} 대신에 ()=>{} 사용이유 : this를 바인딩 하기 위해서!!
 			this.save();
 		});
 	},
@@ -13,6 +13,8 @@ let index = {
 			password : $("#password").val(),
 			email : $("#email").val(),
 		}
+		
+		//$.ajax().done().fail()
 		
 		$.ajax().done().fail(); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!!
 	}
