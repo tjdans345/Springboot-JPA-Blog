3290fd32b2f9bd3342f9e4ff8eea8145e0234c4a
tjdans345 <tjdans34568@gmail.com>
2021-07-31 23:59:40 +0900

    api controller setting

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -24,7 +24,7 @@ public class UserApiController {
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
 		user.setRole(RoleType.USER);
 		userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(JACKSON)
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
 
 }
